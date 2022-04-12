f32ca247c2a4ef4f2a4e62357d6599b861e2f4f2
pouryourlove <lqu134@naver.com>
2022-04-12 17:47:15 +0900

    components

--- index.js
+++ index.js
@@ -68,17 +68,71 @@ import ReactDOM from "react-dom";
 
 function Page() {
   return (
-    <nav>
-      <img src="./react-logo.png" width="40px" />
-      <h1>Reasons why I'm excited to learn React</h1>
+    <div>
+      <header>
+        <nav>
+          <img src="./react-logo.png" width="40px" />
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>
           It's a popular library, so I'll be able to fit in with the cool kids!
         </li>
         <li>I'm more likely to get a job as a developer if I know React</li>
       </ol>
-      <footer> "© 20xx lee development. All rights reserved."</footer>
-    </nav>
+      <footer>© 2021 Ziroll development. All rights reserved.</footer>
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
+
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Ziroll development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </div>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
   );
 }
 
