29dcf88ae877b1594a5271d140790e4584b91505
pouryourlove <lqu134@naver.com>
2022-04-19 00:52:56 +0900

    completed

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Group from "./Group.png";
 
 export default function Main() {
   return (
-    <div className="main">
-      <h1 className="main--title">Fun facts about React</h1>;
+    <div
+      className="main"
+      style={{
+        backgroundImage: `url(${Group})`,
+      }}
+    >
+      <h1 className="main--title">Fun facts about React</h1>
       <ul className="main--text">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
